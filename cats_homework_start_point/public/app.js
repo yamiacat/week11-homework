b2a302454d8385be89f510cef59997cb2e24fb03
Matt Rose <yamiacat@gmail.com>
2017-06-19 18:45:37 +0100

    Add function to add cat

--- cats_homework_start_point/public/app.js
+++ cats_homework_start_point/public/app.js
@@ -1,29 +1,58 @@
 var app = function() {
 
-var catUl = document.createElement("ul");
-catUl.classList.add("cat");
 
-var nameLi = document.createElement("li");
-nameLi.innerText = "Name: Fiona";
+var addCat = function(name, favouriteFood, imgURL) {
+  var catUl = createCatElement();
+  var nameLi = createNameElement(name);
+  var foodLi = createFoodElement(favouriteFood);
+  var picLi = createPicElement(imgURL);
 
-var foodLi = document.createElement("li");
-foodLi.innerText = "Favourite food: Human disappointment";
-
-var picLi = document.createElement("li");
-
-var picImg = document.createElement("img");
-picImg.src = "http://www.wbir.com/img/resize/content.wbir.com/photo/2017/04/07/BABY%20HIPPO_1491561648337_9180558_ver1.0.jpg?preset=534-401";
-picImg.width = "500";
-
-picLi.appendChild(picImg);
-
-catUl.appendChild(nameLi);
-catUl.appendChild(foodLi);
-catUl.appendChild(picLi);
+  var catList = document.querySelector("#cats");
+  appendElements(catList, catUl, nameLi, foodLi, picLi);
+  };
 
 
-var catList = document.querySelector("#cats");
-catList.appendChild(catUl);
+var createCatElement = function() {
+  var catUl = document.createElement("ul");
+  catUl.classList.add("cat");
+  return catUl;
+};
+
+var createNameElement = function(name) {
+  var nameLi = document.createElement("li");
+  nameLi.innerText = "Name: " + name;
+  return nameLi;
+};
+
+var createFoodElement = function(favouriteFood) {
+  var foodLi = document.createElement("li");
+  foodLi.innerText = "Favourite food: " + favouriteFood;
+  return foodLi;
+};
+
+var createPicElement = function(imgURL) {
+  var picLi = document.createElement("li");
+  var picImg = document.createElement("img");
+  picImg.src = imgURL;
+  picImg.width = "500";
+  picLi.appendChild(picImg);
+  return picLi;
+};
+
+var appendElements = function(catList, catUl, nameLi, foodLi, picLi) {
+  catUl.appendChild(nameLi);
+  catUl.appendChild(foodLi);
+  catUl.appendChild(picLi);
+  catList.appendChild(catUl);
+};
+
+
+
+
+
+
+addCat("Fiona", "Human disappointment", "http://www.wbir.com/img/resize/content.wbir.com/photo/2017/04/07/BABY%20HIPPO_1491561648337_9180558_ver1.0.jpg?preset=534-401");
+
 
 };
 
