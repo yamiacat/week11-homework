67211e70c364be10a57474e4bf2d45a687417601
Matt Rose <yamiacat@gmail.com>
2017-06-19 18:51:09 +0100

    Add all cats via function

--- cats_homework_start_point/brief.md
+++ cats_homework_start_point/brief.md
@@ -2,8 +2,8 @@
 
 Use the provided start_point and complete the following tasks:
 
-1. Add JS code to allow a new cat to be added programmatically. Do it rough and ready at first!
-2. Refactor the code to make it easy to add a new cat, just like we did with the quotes.
+<!-- 1. Add JS code to allow a new cat to be added programmatically. Do it rough and ready at first! -->
+<!-- 2. Refactor the code to make it easy to add a new cat, just like we did with the quotes. -->
 3. EXTENSION: Remove the cat data in the HTML and add them via your javascript. If you want to be fancy, put the cat data into an array.
 4. EXTENSION: Style the site!
 
--- cats_homework_start_point/public/app.js
+++ cats_homework_start_point/public/app.js
@@ -48,8 +48,11 @@ var appendElements = function(catList, catUl, nameLi, foodLi, picLi) {
 
 
 
+addCat("Boba", "Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
 
+addCat("Barnaby", "Tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
 
+addCat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
 
 addCat("Fiona", "Human disappointment", "http://www.wbir.com/img/resize/content.wbir.com/photo/2017/04/07/BABY%20HIPPO_1491561648337_9180558_ver1.0.jpg?preset=534-401");
 
