221e9db8a9d1abe4ecf5415206583959c02e108b
Matt Rose <yamiacat@gmail.com>
2017-06-25 12:13:22 +0100

    Artist release button working, though not hooked up to display yet

---
+++ api_map_homework/public/artistClick.js
@@ -0,0 +1,33 @@
+var getArtistAlbums = function(artistID) {
+
+console.log("getArtistAlbums Clicked", artistID);
+
+var url = "https://api.musixmatch.com/ws/1.1/artist.albums.get?format=jsonp&callback=callback&page_size=100&artist_id=" + artistID + "&s_release_date=desc&g_album_name=1&apikey=";
+
+
+var request = new XMLHttpRequest();
+request.open("GET", url + apiKey);
+
+request.setRequestHeader(
+  "content-type", "text/plain; charset=utf-8"
+);
+
+request.addEventListener("load", function() {
+//GRAB JSONP AND EXTRACT JSON
+  var jsonpString = request.responseText;
+  var re = /.*?(?={"album_list)/;
+  var jsonString = jsonpString.replace(re, "");
+  var jsonStringTrimmed = jsonString.slice(0, -4);
+  // console.log("jsonString", jsonString);
+
+//CREATE JSON OBJECTS
+  var albumData = JSON.parse(jsonStringTrimmed);
+
+console.log(albumData);
+
+
+});
+
+request.send();
+
+}
--- api_map_homework/public/renderArtistChart.js
+++ api_map_homework/public/renderArtistChart.js
@@ -1,6 +1,6 @@
 var renderArtistChart = function(chartObject, country) {
 
-//CREATE ARTISTCHART ELEMENT
+  //CREATE ARTISTCHART ELEMENT
   var olBody = document.createElement("ol");
   olBody.classList = country.country + "Chart";
   var title = document.createElement("h2");
@@ -12,12 +12,21 @@ var renderArtistChart = function(chartObject, country) {
   var genreArray = [];
 
   for(var i = 0; i< chartObject.artist_list.length; i++) {
-//POPULATE CHART WITH ARTIST LIST
+    //POPULATE CHART WITH ARTIST LIST
     var artistListing = document.createElement("li");
     artistListing.innerText = chartObject.artist_list[i].artist.artist_name;
+
+    //BUTTON FOR ALBUMS
+    var albumButton = document.createElement("button");
+    albumButton.innerText = "Ugh";
+    albumButton.value = chartObject.artist_list[i].artist.artist_id;
+    albumButton.addEventListener("click", function() {
+      getArtistAlbums(this.value);
+    })
+    artistListing.appendChild(albumButton);
     olBody.appendChild(artistListing);
 
-//RECORD NUMBER OF INSTANCES OF EACH GENRE
+    //RECORD NUMBER OF INSTANCES OF EACH GENRE
     if(chartObject.artist_list[i].artist.primary_genres.music_genre_list.length > 0) {
       var genre = chartObject.artist_list[i].artist.primary_genres.music_genre_list[0].music_genre.music_genre_name;
     } else {
@@ -25,31 +34,27 @@ var renderArtistChart = function(chartObject, country) {
     }
     artistGenreCount[genre] = artistGenreCount[genre] ? artistGenreCount[genre]+1 : 1;
   }
-console.log("artistgenrecount", artistGenreCount);
 
-var cleansedGenreData = [];
+  //CLEANSE GENRE COUNT TO PIECHART FORMAT
 
-for(genre in artistGenreCount) {
-  var dataObject = {}
-  dataObject.name = genre;
-  dataObject.y = artistGenreCount[genre];
+  var cleansedGenreData = [];
 
-  cleansedGenreData.push(dataObject);
-}
-console.log("cleanseddata", cleansedGenreData);
+  for(genre in artistGenreCount) {
+    var dataObject = {}
+    dataObject.name = genre;
+    dataObject.y = artistGenreCount[genre];
 
+    cleansedGenreData.push(dataObject);
+  }
 
-//CREATE PIECHART
+  //CREATE PIECHART
   var pieChartButton = document.createElement("button");
   pieChartButton.innerText = "Genre Breakdown";
   pieChartButton.addEventListener("click", function() {
-
     createPieChart(chartObject, country, cleansedGenreData)
   });
 
   var createPieChart = function(chartObject, country, cleansedGenreData) {
-    console.log("Piechart Button clicked");
-    console.log("country inside createPieChart", country);
 
     new PieChart("Genre Breakdown of " + country.countryName, "Awful Artists", cleansedGenreData);
   }
